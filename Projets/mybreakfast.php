1a13b360382b6345c466e456a75918df3bc07dc7
Saad CHAUDHRY <32572608+Decimoo@users.noreply.github.com>
2021-02-09 00:26:23 +0100

    Update mybreakfast.php
    Fini

--- Projets/mybreakfast.php
+++ Projets/mybreakfast.php
@@ -11,7 +11,6 @@
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="font/font/css/all.min.css">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="author" content="Suriyah Lucien Thevarajah">
 
 	<link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png">
@@ -20,6 +19,13 @@
 	<link rel="mask-icon" href="image/safari-pinned-tab.svg" color="#5bbad5">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">
+	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 	<style>
       /* Always set the map height explicitly to define the size of the div
@@ -37,6 +43,8 @@
 
 </head>
 <?php
+
+	$text="";
     if(isset($_GET['action'])=='test') 
 	{
 					$val = $_POST['filtrage'];
@@ -63,24 +71,14 @@
 	
 ?>
 <body>
-
 	<header>
 		<nav class="menu container">
 			<div>
-				<a href="index.html">
-					<img src="image/logo typo breakfast 2.png"
+				<a href=#>
+					<img src="image/logo_typo_breakfast_2.png"
 					alt= "WELCOME TO BREAK'FAST" 
 					width= "150"></a>
 				</div>
-				<ul class="menu-list">
-					<li>
-						<a class="text-danger" href="index.html">Home</a>
-					</li>
-					
-					<li>
-						<a class="fas fa-user-circle" href="LOGIN.html">Login</a>
-					</li>
-				</ul>
 			</nav>
 			
 		</header>
@@ -88,9 +86,15 @@
 		<?php 
 			$mongo = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 			$filter =[];
-			// new MongoDB\Driver\Command(['fields' => 1]);
 			$options = ['sort' => [$text => 1]]; 
-			$query = new MongoDB\Driver\Query($filter, $options);
+			if ($text)
+			{
+				$query = new MongoDB\Driver\Query($filter, $options);
+			}
+			else
+			{
+				$query = new MongoDB\Driver\Query($filter);
+			}
 			$rows = $mongo->executeQuery('Vin.production', $query); // $mongo contains the connection object to MongoDB
 			$tab[] = array();
 			foreach($rows as $r)
@@ -98,57 +102,59 @@
 				array_push($tab, $r);
 			}
 			
+			
 		?>
 			<form method="post" action="?action=test">
-				<select name = "filtrage">
+				<select style="margin-left:100px;" class="btn btn-secondary dropdown-toggle" name = "filtrage">
 					<option value = "departement">Département</option>
 					<option value = "produit">Produit</option>
 					<option value = "theme">Thème</option>
 					<option value = "annee">Année</option>
 				</select>
-				<button type=submit>Submit</button>
-			</form>
-			<table>
-				<thead>
-					<tr>
-						<td>Infos</td>
-						<td>Département</td>
-						<td>Produit</td>
-						<td>Thème</td>
-						<td>Année</td>
-					</tr>
-				</thead>
-					<?php
-						foreach($tab as $r)
-						{
-							$ide = $r->recordid;
-							if($r)
+				<button class="btn btn-outline-secondary" type=submit>Submit</button>
+			</form><br>
+			<div style='height: 450px; overflow-y:scroll'> 
+				<table class="table-index">
+					<thead>
+						<tr>
+							<td class="saad">Infos</td>
+							<td class="saad">Département</td>
+							<td class="saad">Produit</td>
+							<td class="saad">Thème</td>
+							<td class="saad">Année</td>
+						</tr>
+					</thead>
+						<?php
+							foreach($tab as $r)
 							{
-								try
+								$ide = $r->recordid;
+								if($r)
 								{
-									?>
-									<form method="post" action = "test.php">
+									try
+									{
+										?>
 										<tr>
-											<td>
-												<button name="id" value="<?php echo  $ide ?>">Voir</button>
-												<input type="submit" style="display:none">
-											</td>
-											<td> <?php echo $r->fields->departement;?></td>
-											<td> <?php echo $r->fields->produit;?></td>
-											<td> <?php echo $r->fields->theme;?></td>
-											<td> <?php echo $r->fields->annee;?></td>
-										</tr>
-									</form><?php
+												<td class="saad">
+													<form method="post" action = "test.php"><button class="btn btn-outline-secondary" type="submit">Voir</button>
+													<input name="id" value="<?php echo  $ide ?>"  style="display:none"></form>
+												</td>
+												<td class="saad"> <?php echo $r->fields->departement;?></td>
+												<td class="saad"> <?php echo $r->fields->produit;?></td>
+												<td class="saad"> <?php echo $r->fields->theme;?></td>
+												<td class="saad"> <?php echo $r->fields->annee;?></td>
+											
+										</tr><?php
+										}
+									catch(Exception $e)
+									{
+										var_dump($e);
 									}
-								catch(Exception $e)
-								{
-									var_dump($e);
 								}
 							}
-						}
-					?>
-				
-			</table>
+						?>
+					
+				</table>
+			</div>
 			
 
 
@@ -159,12 +165,4 @@
 
 
 		</body>
-		<footer>
-			<div class="a container">
-				<div >
-					<a class="slogan">
-						<p class="text-light">/Nutrtion gang</p></a>			
-						<p class="text-light">Suivez nous</p>
-						
-									</footer>
 									</html>
